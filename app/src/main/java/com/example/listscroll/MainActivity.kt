169a395e259adcc71e7590041dbc3e1cf69cadef
cuong-dm214948 <cuong.dm214948@sis.hust.edu.vn>
2023-10-27 09:23:22 +0700

    first commit

---
+++ app/src/main/java/com/example/listscroll/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.listscroll
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.example.listscroll.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView : TextView = findViewById(R.id.textview)
+        textView.text = Datasource().loadAffirmations().size.toString()
+    }
+
+}
